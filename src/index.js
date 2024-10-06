ebc42ecb3c4bb0be54f8407d7d1b1d42de94b88a
1mole.coco@gmail.com <1mole.coco@g,gmail.com>
2024-10-06 20:42:20 +0100

    feat: version function

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+
+export default function version () {
+    const cwd = process.cwd();
+    const pkgPath = resolve(cwd, 'package.json');
+    const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
+    return pkg.version;
+};
+
+console.log(version());
